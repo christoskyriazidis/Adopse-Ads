d6c52af043d3ec9fa1773b24e460c37ce8cbd1d9
christoskyriazidis <52704116+christoskyriazidis@users.noreply.github.com>
2021-03-16 14:44:35 +0200

    testing some stuff

---
+++ IdentityServer/Configuration.cs
@@ -0,0 +1,65 @@
+﻿using IdentityModel;
+using IdentityServer.Models;
+using IdentityServer4.EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public static class Configuration
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource
+                {
+                    Name="credentials",
+                    UserClaims =
+                    {
+                        "username",ClaimTypes.Email,"Mobile","role",ClaimTypes.Role,ClaimTypes.NameIdentifier,"lastName",ClaimTypes.StreetAddress,"name","lastName"
+                    }
+                }
+                //new IdentityResources.Email
+            };
+
+        //gia api identify
+        public static IEnumerable<ApiResource> GetApis() =>
+            //kai kala kanoume register ena api pou exei onoma apione google
+            //edw vazoume claims gia to access token..
+            new List<ApiResource> {
+                new ApiResource("ResourceServer","myClaims", new string[]{"username",ClaimTypes.Email,"Mobile","role",ClaimTypes.Role,ClaimTypes.NameIdentifier,"lastName",ClaimTypes.StreetAddress,"name","lastName"}) ,
+                new ApiResource("ApiTwoTest") ,
+            };
+
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client_id_js",
+
+                    AllowedGrantTypes=GrantTypes.Implicit,
+
+                    RedirectUris ={"https://localhost:44366/home/signin"},
+                    PostLogoutRedirectUris ={"https://localhost:44366/Home/Index"},
+                    AllowedCorsOrigins={"https://localhost:44364"},
+                    AllowedScopes =
+                    {
+                        IdentityServer4.IdentityServerConstants.StandardScopes.OpenId,
+                        "ResourceServer",
+                        "credentials"
+                    },
+
+                    AccessTokenLifetime=1,
+                    AllowAccessTokensViaBrowser = true,
+                    RequireConsent = false,
+                }
+            };
+    }
+}
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,21 +1,72 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using IdentityServer.Data;
 
 namespace IdentityServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(config => {
+                config.UseInMemoryDatabase("Memory");
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 4;
+                config.Password.RequireDigit = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+
+                //config.Lockout.MaxFailedAccessAttempts = 4;
+                //config.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+                //config.SignIn.RequireConfirmedEmail = true;
+            }).AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(config => {
+                config.Cookie.Name = "IdentityServer.Cookie";
+                config.LoginPath = "/Auth/Login";
+                config.LogoutPath = "/Auth/Logout";
+                config.AccessDeniedPath = "/Auth/UserAccessDenied";
+            });
+
+            services.AddIdentityServer()
+              //to paketo identity server 4 asp 
+              .AddAspNetIdentity<IdentityUser>()
+              .AddInMemoryApiResources(Configuration.GetApis())
+              .AddInMemoryIdentityResources(Configuration.GetIdentityResources())
+              .AddInMemoryClients(Configuration.GetClients())
+              .AddDeveloperSigningCredential();
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,14 +76,20 @@ namespace IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseStaticFiles();
-            
+
+            app.UseCors("MyPolicy");
+
             app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
             });
+
         }
     }
 }
